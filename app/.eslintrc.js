1896fcfa2340ddd0353f15552afde3309b3ff523
James Barwell <jb@jamesbarwell.co.uk>
2019-10-29 12:52:46 +0000

    Add prettier

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,10 +1,9 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint'
-  ],
   extends: [
-    'plugin:@typescript-eslint/recommended'
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
   ],
   parserOptions: {
     ecmaVersion: 2018,
